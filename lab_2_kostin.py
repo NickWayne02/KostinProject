9d1ab4c428f326949c69d86df6232153588f10e2
Eugene <zheniakost2002@gmail.com>
2023-05-24 13:36:09 +0300

    My second commit

---
+++ lab_2_kostin.py
@@ -0,0 +1,34 @@
+#1
+print("Завдання 1\n")
+print("Programing", "Essentials", "in", sep="***", end="...")
+print("Python")
+print("\n")
+
+#2
+print("Завдання 2\n")
+
+print("    *  \n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *   *\n  *****\n")
+print("\n")
+
+#3
+print("Завдання 3\n")
+
+print("I`m student")
+print("\n")
+
+#4
+print("Завдання 4\n")
+
+print("\"I`m\"\n\"\"learning\"\"\n\"\"\"Python\"\"\"")
+print("\n")
+
+#5
+print("Завдання 5\n")
+
+print(0o555)
+print("\n")
+
+#6
+print("Завдання 6\n")
+
+print(0x777)
